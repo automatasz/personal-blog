dc52b00e82e8d203b031d3825c541eac9ebf05aa
Matas <mataspss@gmail.com>
2025-09-11 12:53:31 +0300

    add getstats actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,10 +2,12 @@ import { getBatch } from "./getBatch";
 import { checkEventComplete } from "./checkEventComplete";
 import { getBatches } from "./getBatches";
 import { postFileIds } from "./postFileIds";
+import { getStats } from "./getStats";
 
 export const server = {
   getBatch,
   checkEventComplete,
   getBatches,
   postFileIds,
+  getStats,
 };
