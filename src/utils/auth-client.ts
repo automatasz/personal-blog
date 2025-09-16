9154248f4046081cb4194a640b1fcbd9cfd9fd48
Matas <mataspss@gmail.com>
2025-09-17 00:49:57 +0300

    feat hide links that require auth

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,7 +1,12 @@
 import { createAuthClient } from "better-auth/svelte";
+import { createAuthClient as createAuthClientVanilla } from "better-auth/client";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "@utils/auth";
 
 export const authClient = createAuthClient({
+  plugins: [inferAdditionalFields<typeof auth>()],
+});
+
+export const authClientVanilla = createAuthClientVanilla({
   plugins: [inferAdditionalFields<typeof auth>()],
 });
