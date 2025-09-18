cd02a86cc5dca6b37141fe05f0d03183afa0be03
Matas <mataspss@gmail.com>
2025-09-18 10:59:20 +0300

    use vercel env vars

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,5 @@
 import { betterAuth } from "better-auth";
-import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_CLIENT_SECRET } from "astro:env/server";
+import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_CLIENT_SECRET, VERCEL_BRANCH_URL } from "astro:env/server";
 import { dialect } from "@utils/db";
 
 export const auth = betterAuth({
@@ -41,5 +41,5 @@ export const auth = betterAuth({
       clientSecret: GOOGLE_AUTH_CLIENT_SECRET,
     },
   },
-  trustedOrigins: ["https://*.vercel.app"],
+  trustedOrigins: VERCEL_BRANCH_URL ? [VERCEL_BRANCH_URL] : undefined,
 });
